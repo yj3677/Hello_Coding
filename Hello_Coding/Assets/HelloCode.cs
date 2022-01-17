9da2af1d5ede54343fe021411952420dfece5f14
yj3677 <dudwn3677@gmail.com>
2022-01-17 12:40:44 +0900

    20220117
    f

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class HelloCode : MonoBehaviour
 {
-    
+    public float distance;
     void Start()
     {
-        Debug.Log("Hello!World");
+        distance = GetDistance(2, 2, 5, 6);
+        Debug.Log("(2,2)에서 (5,6)까지의 거리:" + distance);
     }
+    public float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float width = x2 - x1;
+        float height = y2 - y1;
 
-   
+        distance = width * width + height * height;
+        distance = Mathf.Sqrt(distance);
+        return distance;
+    }
+    
+
 }
