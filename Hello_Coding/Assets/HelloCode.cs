b43a5da694827d0abd3a486ce5fd606fe99e4223
yj3677 <dudwn3677@gmail.com>
2022-01-19 11:39:58 +0900

    Hello_Class
    d

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -15,11 +15,11 @@ public class HelloCode : MonoBehaviour
         students[3] = 70;
         students[4] = 60;
 
-        Debug.Log("0 번 학생의 점수: " + students[0]);
-        Debug.Log("1 번 학생의 점수: " + students[1]);
-        Debug.Log("2 번 학생의 점수: " + students[2]);
-        Debug.Log("3 번 학생의 점수: " + students[3]);
-        Debug.Log("4 번 학생의 점수: " + students[4]);
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log((i + 1) + "번 학생의 점수:" + students[i]);
+        }
+       
     }
   
     
